628dbccf9a33deef75c541cc4e9015de506caf59
Eduardo Portet <eduardoportet@gmail.com>
2019-02-01 21:40:33 -0800

    react router

---
+++ src/pages/Work/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class WorkPage extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        To change this page go to <code>/src/pages/Work/index.js</code>
+      </div>
+    )
+  }
+}
+
+export default WorkPage
