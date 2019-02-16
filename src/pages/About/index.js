ee9bd0d37e95ebadc22fc6981b4ce86c1780a9b1
Mary Liang <maryliangf@gmail.com>
2019-02-16 13:51:27 -0800

    init

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
-import CallitWhatYouWant from '../../assets/IMG_4980.JPG'
 import { Image, Label, Grid, Header, List } from 'semantic-ui-react'
-import TopTop from '../../components/Container'
 
 import PhiloHT from './Philo'
+import TopTop from '../../components/Container'
 import Hero from '../../components/Hero'
+import CallitWhatYouWant from '../../assets/IMG_4980.JPG'
 
 class AboutPage extends Component {
-
   render() {
     return (
       <TopTop location={this.props.location}>
@@ -40,157 +37,157 @@ class AboutPage extends Component {
           />
         </Hero>
 
-        <Grid centered
-          style={{ margin: '5rem 15rem' }}>
+        <div style={{ maxWidth: '1440px', margin: '0 auto' }}>
+          <Grid centered
+            style={{ margin: '5rem 15rem' }}>
 
-          <Grid.Column width={6}>
-            <Image src={CallitWhatYouWant} size='medium' />
-          </Grid.Column>
+            <Grid.Column width={6}>
+              <Image src={CallitWhatYouWant} size='medium' />
+            </Grid.Column>
 
-          <Grid.Column width={10}
-            style={{ padding: '0.8rem 0', paddingLeft: '10rem' }}
-          >
-            <Header style={{ letterSpacing: '0.3rem' }}>
-              MEET MARY
+            <Grid.Column width={10}
+              style={{ padding: '0.8rem 0', paddingLeft: '10rem' }}
+            >
+              <Header style={{ letterSpacing: '0.3rem' }}>
+                MEET MARY
             </Header>
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam.
             <br />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam.
-            <br />
-            <br />
-            Nullam eget dolor semper, imperdiet nulla id, maximus nunc. Duis euismod vehicula maximus. Morbi varius dolor vel sollicitudin egestas. Fusce risus ex, tincidunt at placerat non, fermentum eget nisi. Mauris condimentum est augue, a maximus magna auctor ac. Nulla vel quam eros. Nunc id ultricies elit. Ut quis dolor neque. Praesent id sapien tellus. Aliquam erat volutpat. Sed auctor lorem nisl, sit amet mattis dolor lobortis eget
+              <br />
+              Nullam eget dolor semper, imperdiet nulla id, maximus nunc. Duis euismod vehicula maximus. Morbi varius dolor vel sollicitudin egestas. Fusce risus ex, tincidunt at placerat non, fermentum eget nisi. Mauris condimentum est augue, a maximus magna auctor ac. Nulla vel quam eros. Nunc id ultricies elit. Ut quis dolor neque. Praesent id sapien tellus. Aliquam erat volutpat. Sed auctor lorem nisl, sit amet mattis dolor lobortis eget
           </Grid.Column>
-        </Grid>
+          </Grid>
 
-        <div
-          style={{
-            borderBottom: '2px solid black',
-            width: '15rem',
-            margin: '0 auto'
-          }}>
-        </div>
+          <div
+            style={{
+              borderBottom: '2px solid black',
+              width: '15rem',
+              margin: '0 auto'
+            }}>
+          </div>
 
-        <div
-          style={{
-            fontSize: '1.7rem',
-            fontFamily: 'lato',
-            fontWeight: 'bold',
-            letterSpacing: '0.5rem',
-            textAlign: 'center',
-            marginTop: '5rem',
-          }}>
-          MY WORK PHILOSOPHY
+          <div
+            style={{
+              fontSize: '1.7rem',
+              fontFamily: 'lato',
+              fontWeight: 'bold',
+              letterSpacing: '0.5rem',
+              textAlign: 'center',
+              marginTop: '5rem',
+            }}>
+            MY WORK PHILOSOPHY
 </div>
 
-        <Grid centered
-          style={{ margin: '2rem 15rem' }}>
+          <Grid centered
+            style={{ margin: '2rem 15rem' }}>
 
-          <Grid.Column width={8}>
-            <PhiloHT left
-              title="PHILOSOPHY 1"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-          </Grid.Column>
+            <Grid.Column width={8}>
+              <PhiloHT left
+                title="PHILOSOPHY 1"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <PhiloHT
-              title="PHILOSOPHY 2"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-            <br />
+            <Grid.Column width={8}>
+              <PhiloHT
+                title="PHILOSOPHY 2"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+              <br />
 
-          </Grid.Column>
-        </Grid>
+            </Grid.Column>
+          </Grid>
 
-        <Grid centered
-          style={{ margin: '2rem 15rem' }}>
+          <Grid centered
+            style={{ margin: '2rem 15rem' }}>
 
-          <Grid.Column width={8}>
-            <PhiloHT left
-              title="PHILOSOPHY 3"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-          </Grid.Column>
+            <Grid.Column width={8}>
+              <PhiloHT left
+                title="PHILOSOPHY 3"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <PhiloHT
-              title="PHILOSOPHY 4"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-            <br />
+            <Grid.Column width={8}>
+              <PhiloHT
+                title="PHILOSOPHY 4"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+              <br />
 
-          </Grid.Column>
-        </Grid>
+            </Grid.Column>
+          </Grid>
 
-        <Grid centered
-          style={{ margin: '2rem 15rem' }}>
+          <Grid centered
+            style={{ margin: '2rem 15rem' }}>
 
-          <Grid.Column width={8}>
-            <PhiloHT left
-              title="PHILOSOPHY 5"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-          </Grid.Column>
+            <Grid.Column width={8}>
+              <PhiloHT left
+                title="PHILOSOPHY 5"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <PhiloHT
-              title="PHILOSOPHY 6"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
-            />
-            <br />
+            <Grid.Column width={8}>
+              <PhiloHT
+                title="PHILOSOPHY 6"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
+              />
+              <br />
 
-          </Grid.Column>
-        </Grid>
+            </Grid.Column>
+          </Grid>
 
-        <div
-          style={{
-            borderBottom: '2px solid black',
-            width: '15rem',
-            margin: '0 auto'
-          }}>
-        </div>
+          <div
+            style={{
+              borderBottom: '2px solid black',
+              width: '15rem',
+              margin: '0 auto'
+            }}>
+          </div>
 
-        <div
-          style={{
-            fontSize: '1.7rem',
-            fontFamily: 'lato',
-            fontWeight: 'bold',
-            letterSpacing: '0.5rem',
-            textAlign: 'center',
-            marginTop: '5rem',
-          }}>
-          MY CAREER GOALS
+          <div
+            style={{
+              fontSize: '1.7rem',
+              fontFamily: 'lato',
+              fontWeight: 'bold',
+              letterSpacing: '0.5rem',
+              textAlign: 'center',
+              marginTop: '5rem',
+            }}>
+            MY CAREER GOALS
 </div>
 
-        <Grid centered
-          style={{ margin: '3rem 15rem' }}>
+          <Grid centered
+            style={{ margin: '3rem 15rem' }}>
 
-          <Grid.Column width={8}>
-            <PhiloHT
-              title="SHORT TERM">
-              <List relaxed>
-                <List.Item>Short term career goal 1</List.Item>
-                <List.Item>Short term career goal 2</List.Item>
-                <List.Item>Short term career goal 3</List.Item>
-              </List>
-            </PhiloHT>
-          </Grid.Column>
+            <Grid.Column width={8}>
+              <PhiloHT
+                title="SHORT TERM">
+                <List relaxed>
+                  <List.Item>Short term career goal 1</List.Item>
+                  <List.Item>Short term career goal 2</List.Item>
+                  <List.Item>Short term career goal 3</List.Item>
+                </List>
+              </PhiloHT>
+            </Grid.Column>
 
-          <Grid.Column width={8}>
-            <PhiloHT
-              title="LONG TERM">
-              <List relaxed>
-                <List.Item>Long term career goal 1</List.Item>
-                <List.Item>Long term career goal 2</List.Item>
-                <List.Item>Long term career goal 3</List.Item>
-              </List>
-            </PhiloHT>
-            <br />
+            <Grid.Column width={8}>
+              <PhiloHT
+                title="LONG TERM">
+                <List relaxed>
+                  <List.Item>Long term career goal 1</List.Item>
+                  <List.Item>Long term career goal 2</List.Item>
+                  <List.Item>Long term career goal 3</List.Item>
+                </List>
+              </PhiloHT>
+              <br />
 
-          </Grid.Column>
-        </Grid>
+            </Grid.Column>
+          </Grid>
 
-
-
+        </div>
       </TopTop >
     )
   }
--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import { Label, Grid, Header, Rail, Sticky, Segment } from 'semantic-ui-react'
+import { Label, Grid, Rail, Sticky, Menu } from 'semantic-ui-react'
 
 import TopTop from '../../components/Container'
 import Hero from '../../components/Hero'
 import BGExperience from '../../assets/Fruitwhitegraybg.jpg'
-import HeaderSubHeader from 'semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader';
 
 import UCSF from '../../assets/ucsf.jpg'
 import ExpHT from './Exp'
@@ -44,39 +43,42 @@ class ExperiencePage extends Component {
 					/>
 				</Hero>
 
-				<Grid centered style={{ margin: '3rem' }}>
-					<Grid.Column width={4} />
-					<Grid.Column width={12}>
-						<div>
-							<ExpHT
-								srcimage={UCSF}
-								title="TITLE EXPERIENCE 1"
-								subtitle="Role Experience 1 | Date Experience 1"
-								text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam."
-							/>
+				<div style={{ maxWidth: '1440px', margin: '0 auto' }}>
 
-							<ExpHT
-								srcimage={UCSF}
-								title="TITLE EXPERIENCE 2"
-								subtitle="Role Experience 2 | Date Experience 2"
-								text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam."
-							/>
-						</div>
-						<div ref={this.handleContextRef}>
-							<Rail position='left'>
-								<Sticky context={contextRef} offset={80}>
-									<Segment>
-										<Header content="Experience 1" />
-										<Header content="Experience 2" />
-										<Header content="Experience 3" />
-										<Header content="Experience 4" />
-										<Header content="Experience 5" />
-									</Segment>
-								</Sticky>
-							</Rail>
-						</div>
-					</Grid.Column>
-				</Grid >
+					<Grid centered style={{ margin: '3rem' }}>
+						<Grid.Column width={4} />
+						<Grid.Column width={12}>
+							<div>
+								<ExpHT
+									src={UCSF}
+									title="TITLE EXPERIENCE 1"
+									subtitle="Role Experience 1 | Date Experience 1"
+									text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam."
+								/>
+
+								<ExpHT
+									src={UCSF}
+									title="TITLE EXPERIENCE 2"
+									subtitle="Role Experience 2 | Date Experience 2"
+									text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam."
+								/>
+							</div>
+							<div ref={this.handleContextRef}>
+								<Rail position='left'>
+									<Sticky context={contextRef} offset={160}>
+										<Menu pointing secondary vertical>
+											<Menu.Item name='Experience 1' active={true} />
+											<Menu.Item name='Experience 2' />
+											<Menu.Item name='Experience 3' />
+											<Menu.Item name='Experience 4' />
+											<Menu.Item name='Experience 5' />
+										</Menu>
+									</Sticky>
+								</Rail>
+							</div>
+						</Grid.Column>
+					</Grid >
+				</div>
 			</TopTop >
 
 		)
