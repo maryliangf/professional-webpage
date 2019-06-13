e145dbc16760bcb5178aa4aad3fa0f594702f91e
Eduardo Portet <eduardoportet@gmail.com>
2019-06-12 23:40:54 -0700

    resume and firebase

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
 
 const ResumePage = () => {
-    return <div>
-
-    </div>
+    return <Redirect to='https://drive.google.com/open?id=1D8xn2ok1Bnu9Kzg7A5o5zD-m5sagFUdg' />
 }
 
 export default ResumePage
