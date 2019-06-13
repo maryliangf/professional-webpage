da2932625398c38eac53a63bc07dbd043d9ea2c2
Eduardo Portet <eduardoportet@gmail.com>
2019-06-12 23:55:25 -0700

    resume changes

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect } from 'react-router-dom'
 
 const ResumePage = () => {
-    return <Redirect to='https://drive.google.com/open?id=1D8xn2ok1Bnu9Kzg7A5o5zD-m5sagFUdg' />
+    return <Redirect to='//https://drive.google.com/open?id=1D8xn2ok1Bnu9Kzg7A5o5zD-m5sagFUdg' />
 }
 
 export default ResumePage
