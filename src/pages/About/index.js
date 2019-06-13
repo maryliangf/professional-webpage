0cd8472741432467885cad9ae999a8c947cf3a00
Mary Liang <maryliangf@gmail.com>
2019-06-12 23:30:14 -0700

    AllSet1

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import { Image, Label, Grid, Header, List } from 'semantic-ui-react'
+import styled from 'styled-components'
 
 import PhiloHT from './Philo'
 import TopTop from '../../components/Container'
 import Hero from '../../components/Hero'
 import CallitWhatYouWant from '../../assets/IMG_4980.JPG'
+
+const IntroP = styled.div`
+  b {
+    font-family: 'Lato'
+  }
+`
 
 class AboutPage extends Component {
   state = {
@@ -53,14 +60,23 @@ class AboutPage extends Component {
               <Image src={CallitWhatYouWant} centered floated={this.state.width > 767 ? 'right' : null} size='medium' />
             </Grid.Column>
             <Grid.Column width={10}>
-              <div style={{ maxWidth: '37rem', margin: 'auto' }}>
+              <IntroP style={{ maxWidth: '37rem', margin: 'auto' }}>
                 <Header style={{ letterSpacing: '0.3rem' }}>MEET MARY</Header>
                 <br />
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex. Suspendisse vel nunc arcu. Aenean id elementum justo. In faucibus convallis dapibus. Ut eu lectus sagittis leo placerat ornare. Duis eget varius orci, a vehicula diam.
+                Hi and welcome to my page!
                 <br />
                 <br />
-                Nullam eget dolor semper, imperdiet nulla id, maximus nunc. Duis euismod vehicula maximus. Morbi varius dolor vel sollicitudin egestas. Fusce risus ex, tincidunt at placerat non, fermentum eget nisi. Mauris condimentum est augue, a maximus magna auctor ac. Nulla vel quam eros. Nunc id ultricies elit. Ut quis dolor neque. Praesent id sapien tellus. Aliquam erat volutpat. Sed auctor lorem nisl, sit amet mattis dolor lobortis eget
-              </div>
+                My name is <b>Mary</b> and I am currently a dietetic intern at University of California, San Francisco Medical Center (UCSF).
+                <br />
+                <br />
+                Though I was born and raised in the <b>Dominican Republic</b>, my parents are immigrants from <b>China</b>. I then moved to the <b>United States</b> to complete my undergraduate degree in <b>Boston, MA</b> and am currently based in <b>San Francisco, CA</b>.
+              <br />
+                <br />
+                I am very passionate about helping others become healthier versions of themselves, especially to prevent chronic diseases through evidence-based medical nutrition therapy. My upbringing in a family that values health and watching my own mother struggle with genetic nutritional conditions have shaped my love for nutrition. As a dietetic intern, I hope to become a <b>Registered Dietitian</b> and an expert at providing medical nutrition therapy for a wide demographics that encompasses diverse cultures, ethnicities, and socioeconomic backgrounds.
+                <br />
+                <br />
+                I also enjoy cooking, traveling, doing yoga, being outdoors, and learning coding.
+              </IntroP>
             </Grid.Column>
           </Grid>
 
@@ -84,10 +100,29 @@ class AboutPage extends Component {
             MY WORK PHILOSOPHY
 </div>
 
-          <Grid centered
-            style={{ margin: '2rem 15rem' }}>
+          <Grid centered>
+            <IntroP style={{ margin: '2rem 15rem' }}>
 
-            <Grid.Column width={8}>
+              <p>
+                I firmly believe in <b>disease prevention and management through nutrition</b>. This is why I want to make a long-lasting difference in the field of dietetics by helping people improve health outcomes by providing <b>medical nutrition therapy through evidence-based practice</b>. I am also passionate about providing this type of therapy to a wide breathe of demographics that includes a variety of cultures, ethnicities, and socioeconomic backgrounds.
+           <br />
+                <br />
+                I am <b>focused, driven, and detail-oriented</b>, which are qualities that serve me well in clinical practice as an RD-to-be. I easily feel sympathy with others and am committed in actively helping others while they seek help, or at least offer my help.
+           <br />
+                <br />
+                I recognize that there is not one glove that fits all and believe that <b>individualization </b>is key to a well-rounded approach to a solution, whether it is for a workshop, a project, or nutrition interventions.
+              <br />
+                <br />
+                As a current dietetic intern and a human-being in the world, I recognize that there is always room for improvement and thus always try to look for ways in which I can grow and better myself. I believe that there is always something good that comes from failure and also believe in the importance of learning from past experiences and thus am always <b>open for feedback</b>.
+              <br />
+                <br />
+                I have seen, from my prior and current experiences, that <b>hard work</b> is key to success and always strive in putting all my dedication, thoroughness, and thought in whatever I am working on.
+            </p>
+              <br />
+              <br />
+              <br />
+            </IntroP>
+            {/* <Grid.Column width={8}>
               <PhiloHT left
                 title="PHILOSOPHY 1"
                 text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui arcu, scelerisque non elit nec, lacinia vestibulum quam. Phasellus sed mattis magna. Integer mattis quis nisi sit amet facilisis. Integer vitae neque vel nisl volutpat varius nec ac ex."
@@ -121,8 +156,8 @@ class AboutPage extends Component {
               />
               <br />
 
-            </Grid.Column>
-          </Grid>
+            </Grid.Column> */}
+            {/* </Grid>
 
           <Grid centered
             style={{ margin: '2rem 15rem' }}>
@@ -141,7 +176,7 @@ class AboutPage extends Component {
               />
               <br />
 
-            </Grid.Column>
+            </Grid.Column> */}
           </Grid>
 
           <div
@@ -171,9 +206,10 @@ class AboutPage extends Component {
               <PhiloHT
                 title="SHORT TERM">
                 <List relaxed>
-                  <List.Item>Short term career goal 1</List.Item>
-                  <List.Item>Short term career goal 2</List.Item>
-                  <List.Item>Short term career goal 3</List.Item>
+                  <List.Item>Practice medical nutrition therapy as an evidence-based practice for a wide demographic through meaningful, empathetic patient care in the inpatient setting </List.Item>
+                  <List.Item>Successfully complete my dietetic internship at UCSF Medical Center</List.Item>
+                  <List.Item>Obtain my Registered Dietitian (RD) certification</List.Item>
+                  <List.Item>Work as a per diem inpatient clinical dietitian at UCSF Medical Center</List.Item>
                 </List>
               </PhiloHT>
             </Grid.Column>
@@ -182,9 +218,10 @@ class AboutPage extends Component {
               <PhiloHT
                 title="LONG TERM">
                 <List relaxed>
-                  <List.Item>Long term career goal 1</List.Item>
-                  <List.Item>Long term career goal 2</List.Item>
-                  <List.Item>Long term career goal 3</List.Item>
+                  <List.Item>Attain a Masters in Science for Nutrition</List.Item>
+                  <List.Item>Obtain specialized certification as a Certified Nutrition Support Clinician</List.Item>
+                  <List.Item>Make a difference helping patients improve health outcomes by providing exceptional care and education with medical nutrition therapy and evidenced-based practice at a teaching hospital</List.Item>
+                  <List.Item>Continue evidence-based practice throughout my career through clinical practice and staying current on research</List.Item>
                 </List>
               </PhiloHT>
               <br />
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -67,7 +67,49 @@ class ContactPage extends Component {
                         fontFamily: 'Lato',
                         fontSize: '1.2rem',
                     }}>
-                    Whether you have questions or are eager to start, I would love to hear from you!
+                    Whether you have questions or comments, I would love to hear from you!
+                </div>
+                <div
+                    style={{
+                        margin: '2.5rem auto',
+                        padding: '2rem 2rem',
+                        width: '24rem',
+                        border: '2px solid black',
+                        borderRadius: '3px'
+                    }}>
+                    <p
+                        style={{
+                            margin: '0',
+                            paddingBottom: '1rem',
+                            textAlign: 'center',
+                            letterSpacing: '0.2rem',
+                            fontWeight: 'bold',
+                            fontFamily: 'Lato',
+                            fontSize: '1.5rem'
+                        }}>
+                        Mary Liang
+                </p>
+
+                    <p
+                        style={{
+                            margin: '0',
+                            textAlign: 'center',
+                            fontFamily: 'Lato',
+                            fontSize: '1.2rem',
+                            paddingBottom: '0.6rem'
+                        }}>
+                        maryliangf@gmail.com
+                    <br />
+                    </p>
+                    <p
+                        style={{
+                            margin: '0',
+                            textAlign: 'center',
+                            fontFamily: 'Lato',
+                            fontSize: '1.2rem',
+                        }}>
+                        (617) - 869 - 6808
+                </p>
                 </div>
 
                 <div
