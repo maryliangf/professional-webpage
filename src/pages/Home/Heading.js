b0345bfbfe8d8f76eaa639b3d85ab636fc0dc4e0
Eduardo Portet <eduardoportet@gmail.com>
2020-02-16 16:06:24 -0800

    RD!!!!

--- src/pages/Home/Heading.js
+++ src/pages/Home/Heading.js
@@ -1,28 +1,22 @@
-import React from 'react'
-import Typed from 'react-typed'
+import React from "react";
+import Typed from "react-typed";
 
-import Hero, { HeroText } from '../../components/Hero'
+import Hero, { HeroText } from "../../components/Hero";
 
 const HomepageHeading = () => (
-  <Hero url='https://images.unsplash.com/photo-1498522271744-cdd435c13f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1054&q=80'>
-    <HeroText
-      color='black'
-      as='h1'
-    >I'm Mary,</HeroText>
+  <Hero url="https://images.unsplash.com/photo-1498522271744-cdd435c13f24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1054&q=80">
+    <HeroText color="black" as="h1">
+      I'm Mary,
+    </HeroText>
     <br></br>
-    <HeroText color='black'
-      as='h1'
-    >a dietetic intern.</HeroText>
+    <HeroText color="black" as="h1">
+      a Registered Dietitian.
+    </HeroText>
     <br></br>
-    <HeroText color='black'
-      as='h2'
-    >
-      <Typed
-        strings={['Welcome to my career portfolio.']}
-        typeSpeed={110}
-      />
+    <HeroText color="black" as="h2">
+      <Typed strings={["Welcome to my career portfolio."]} typeSpeed={110} />
     </HeroText>
   </Hero>
-)
+);
 
-export default HomepageHeading
+export default HomepageHeading;
