1b4fadca3f6cc4d7f86e3017ff6ae679f8bf471c
Eduardo Portet <eduardoportet@gmail.com>
2021-09-26 16:23:08 -0700

    fix wdith

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { useWindowSize } from 'react-use'
 
 import DesktopContainer from './Desktop'
 import { Footer } from './Footer'
 import MobileContainer from './Mobile'
 
 const Responsive = ({ children }) => {
-  const { width } = useWindowSize()
-
-  if (width <= 425) {
+  if (window.outerWidth <= 425) {
     return (
       <MobileContainer>
         {children}
